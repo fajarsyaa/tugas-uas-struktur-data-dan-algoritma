fcc80b92f2daaa6df7ff6aa28071ea794ec6b7f9
Fajarsya <f4j4rkokok@gmail.com>
2023-07-31 11:23:44 +0700

    first commit

---
+++ uas1.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Person {
+    string name;
+    string address;
+};
+
+// Bubble Sort function
+void bubbleSort(Person arr[], int n) {
+    int counter = 1;
+    while(counter<n){
+        for(int i=0; i<n-counter;i++){
+            if(arr[i].name > arr[i+1].name){
+                Person temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+        counter++;
+    }
+}
+
+// Selection Sort function
+void selectionSort(Person arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {        
+        for (int j = i + 1; j < n; j++) {
+            if(arr[j].name < arr[i].name){
+                Person temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }       
+    }
+}
+
+int main() {
+    const int n = 8;
+    Person data[n] = {
+        {"Fahmi", "Jakarta"},
+        {"Romi", "Solo"},
+        {"Andri", "Jakarta"},
+        {"Fadillah", "Banyuwangi"},
+        {"Ruli", "Bandung"},
+        {"Rudi", "Bali"},
+        {"Dendi", "Purwokerto"},
+        {"Zaki", "Madiun"}
+    };
+
+    cout << "Data awal sebelum diurutkan:" << endl;
+    cout << "Nama\t\tAlamat" << endl;
+    cout << "----------------------"<<endl;
+    for (int i = 0; i < n; i++) {
+        cout << data[i].name << "\t\t" << data[i].address << endl;
+    }
+
+    // Sorting using Bubble Sort
+    bubbleSort(data, n);
+    cout << "\nHasil setelah diurutkan dengan Bubble Sort:" << endl;
+    cout << "Nama\t\tAlamat" << endl;
+    cout << "----------------------"<<endl;
+    for (int i = 0; i < n; i++) {
+        cout << data[i].name << "\t\t" << data[i].address << endl;
+    }
+
+    // Sorting using Selection Sort
+    selectionSort(data, n);
+    cout << "\nHasil setelah diurutkan dengan Selection Sort:" << endl;
+    cout << "Nama\t\tAlamat" << endl;
+    cout << "----------------------"<<endl;
+    for (int i = 0; i < n; i++) {
+        cout << data[i].name << "\t\t" << data[i].address << endl;
+    }
+
+    return 0;
+}
---
+++ uas2.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+// Binary Search function
+int cariDenganBinarySearch(const int arr[], int size, int target) {
+    int bawah = 0;
+    int atas = size - 1;
+    int posisi = -1;
+
+    while (atas >= bawah) {
+        int tengah = (atas + bawah) / 2;
+
+        if (target > arr[tengah]) {
+            bawah = tengah + 1;
+        } else if (target < arr[tengah]) {
+            atas = tengah - 1;
+        } else {
+            posisi = tengah;
+            break; 
+        }
+    }
+
+    return posisi;
+}
+
+
+int main() {
+    int arr[] = {19, 40, 10, 90, 2, 50, 60, 50, 1};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    int target;
+    int posisi;
+
+    // Test case 1
+    target = 1;
+    posisi = cariDenganBinarySearch(arr, size, target);    
+    if (posisi >= 0) {
+        cout << "Input: " << target << endl;
+        cout << "Output: Angka " << target << " ada di indeks ke " << posisi << endl;
+    } else {
+        cout << "Input: " << target << endl;
+        cout << "Output: Angka " << target << " tidak ada dalam array" << endl;
+    }
+
+    // Test case 2
+    target = 50;
+    posisi = cariDenganBinarySearch(arr, size, target);
+    cout<<posisi;
+    if (posisi >= 0) {
+        cout << "\nInput: " << target << endl;
+        cout << "Output: Angka " << target << " ada di indeks ke " << posisi << endl;
+    } else {
+        cout << "\nInput: " << target << endl;
+        cout << "Output: Angka " << target << " tidak ada dalam array" << endl;
+    }
+
+    // Test case 3
+    target = 100;
+    posisi = cariDenganBinarySearch(arr, size, target);
+    if (posisi >= 0) {
+        cout << "\nInput: " << target << endl;
+        cout << "Output: Angka " << target << " ada di indeks ke " << posisi << endl;
+    } else {
+        cout << "\nInput: " << target << endl;
+        cout << "Output: Angka " << target << " tidak ada dalam array" << endl;
+    }
+
+    return 0;
+}
